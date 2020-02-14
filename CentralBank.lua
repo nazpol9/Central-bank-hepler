596143668d15b6110513e5d017d508de677850df
nazpol9 <61046385+nazpol9@users.noreply.github.com>
2020-02-14 11:40:26 +0200

    Add files via upload

--- CentralBank.lua
+++ CentralBank.lua
@@ -31,10 +31,13 @@ local ActiveMenu = {
 }
 local bindID = 0
 
+
+
 function main()
 	repeat wait(100) until isSampAvailable()
     if not isSampLoaded() then return end
     while not isSampAvailable() do wait(100) end
+	autoupdate("", '['..string.upper(thisScript().name)..']: ', "arizona-rp.com")
 	sampAddChatMessage('{1288E8}[Bank Script]: {FFFFFF}Скрипт - Central Bank Helper v0.2 {28FF2B}успешно загружен.', -1)
 	sampAddChatMessage('{1288E8}[Bank Script]: {FFFFFF}Для работы пропишите {28FF2B}/cbhelp или нажмите F3', -1)
 	sampRegisterChatCommand('mb', cmd_mb)
@@ -969,4 +972,60 @@ function style_yellow()
     colors[clr.PlotHistogram]          = ImVec4(0.90, 0.70, 0.00, 1.00)
     colors[clr.PlotHistogramHovered]   = ImVec4(1.00, 0.60, 0.00, 1.00)
     colors[clr.ModalWindowDarkening]   = ImVec4(0.80, 0.80, 0.80, 0.35)
+end
+
+function autoupdate(json_url, prefix, url)
+  local dlstatus = require('moonloader').download_status
+  local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+  if doesFileExist(json) then os.remove(json) end
+  downloadUrlToFile(json_url, json,
+    function(id, status, p1, p2)
+      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        if doesFileExist(json) then
+          local f = io.open(json, 'r')
+          if f then
+            local info = decodeJson(f:read('*a'))
+            updatelink = info.updateurl
+            updateversion = info.latest
+            f:close()
+            os.remove(json)
+            if updateversion ~= thisScript().version then
+              lua_thread.create(function(prefix)
+                local dlstatus = require('moonloader').download_status
+                local color = -1
+                sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                wait(250)
+                downloadUrlToFile(updatelink, thisScript().path,
+                  function(id3, status1, p13, p23)
+                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                      print(string.format('Загружено %d из %d.', p13, p23))
+                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                      print('Загрузка обновления завершена.')
+                      sampAddChatMessage((prefix..'Обновление завершено!'), color)
+                      goupdatestatus = true
+                      lua_thread.create(function() wait(500) thisScript():reload() end)
+                    end
+                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                      if goupdatestatus == nil then
+                        sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                        update = false
+                      end
+                    end
+                  end
+                )
+                end, prefix
+              )
+            else
+              update = false
+              print('v'..thisScript().version..': Обновление не требуется.')
+            end
+          end
+        else
+          print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+          update = false
+        end
+      end
+    end
+  )
+  while update ~= false do wait(100) end
 end
